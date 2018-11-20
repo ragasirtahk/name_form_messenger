14cc6ff6bb36dbe4da77dcab5b5f6298208986ac
Sagar <ragasirtahk@gmail.com>
2018-11-20 10:40:30 +0530

    first commit

---
+++ src/Form/NameFormMessenger.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\name_form_messenger\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\MessengerTrait;
+
+/**
+ * Implements form.
+ */
+class NameFormMessenger extends FormBase {
+  use MessengerTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'name_form_messenger';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Name:'),
+    ];
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit!'),
+      '#button_type' => 'primary',
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->messenger()->addMessage($this->t('Name: ' . $form_state->getValue('name')));
+  }
+
+}
---
+++ src/Tests/NameFormMessengerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\name_form_messenger\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Provide some basic tests for our NameFormMessenger form.
+ *
+ * @group name_form
+ */
+class NameFormMessengerTest extends WebTestBase {
+
+  /**
+   * Modules to install.
+   *
+   * @var array
+   */
+  public static $modules = ['node', 'name_form_messenger'];
+
+  /**
+   * Tests that 'name/form_messenger' returns a 200.
+   */
+  public function testNameFormMessengerRouterUrlIsAccessible() {
+    $this->drupalGet('name/form_messenger');
+    $this->assertResponse(200, 'URL is accessible to user.');
+  }
+
+  /**
+   * Tests that the form has a submit button to use.
+   */
+  public function testNameFormMessengerSubmitButtonExists() {
+    $this->drupalGet('name/form_messenger');
+    $this->assertResponse(200);
+    $this->assertFieldById('edit-submit', 'Submit!', 'User can submit the form.');
+  }
+
+  /**
+   * Test the submission of the form.
+   */
+  public function testNameFormMessengerSubmit() {
+    $this->drupalPostForm(
+      'name/form_messenger',
+      [
+        'name' => 'Test Name',
+      ],
+      t('Submit!')
+    );
+
+    $this->assertText('Name: Test Name');
+  }
+
+}
